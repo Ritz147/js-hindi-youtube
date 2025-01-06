16c08c0781d76075a5efcfc30046dbf7c1a7c745
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-06 14:05:06 +0000

    datatypes added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict";//treat all js code as newer version
+//alert("hello") we are using nodejs not browser  
+console.log("hitesh");
+console.log(typeof "hitesh")
+console.log(typeof age);
+console.log(typeof null); 
+console.log(typeof undefined)
