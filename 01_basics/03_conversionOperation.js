66aec30a639709db7fe8c0d060e08d4d4f99849c
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-06 14:05:37 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+/*let score=true;
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber=Number(score);
+console.log(typeof score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+/*let isLoggedIn="";
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)*/
+let someNumber=33;
+let StringNumber=String(someNumber);
+console.log(typeof StringNumber) 
