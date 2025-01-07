7ae1846012c77e2bf7f0b12385278443195db967
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-07 08:01:03 +0000

    datatype_summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -5,14 +5,26 @@
 //  const bigNumber=3455678890123n
 //  console.log(typeof bigNumber)
 //Arrays,Objects,Functions
-const heroes=["Ritu",'Harshit','Rishabh'];
-let myObj={
-    name:'hitesh',
-    age:22
+// const heroes=["Ritu",'Harshit','Rishabh'];
+// let myObj={
+//     name:'hitesh',
+//     age:22
+// }
+// const myfunc=function(){
+//  console.log("Hello World")
+// }
+// console.log(typeof heroes)
+// console.log(typeof myObj)
+// console.log(typeof myfunc)
+// let myName="Ritu"
+// let anotherName=myName
+// anotherName="Harshit"
+// console.log(myName)
+// console.log(anotherName)
+let userOne={
+    email:"ritz.2002@gmail.com",
+    upi:"7296@paytm"
 }
-const myfunc=function(){
- console.log("Hello World")
-}
-console.log(typeof heroes)
-console.log(typeof myObj)
-console.log(typeof myfunc)
+let userTwo=userOne;
+userTwo.email='harshit@gmail.com'
+console.log(userOne.email)
