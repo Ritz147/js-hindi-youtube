a191a16fd9ff85b3d00e7b0086a1d9b64c0eb506
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-07 16:25:53 +0000

    objects part 2 commit

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,34 @@
+// const tinderUser=new Object()
+// console.log(tinderUser)
+const tinderUser={};
+tinderUser.id="123abc"
+tinderUser.name="Sammy"
+tinderUser.isLoggedIn=false
+//console.log(tinderUser)
+// const regularuser={
+//     email:"some@gmail.com",
+//     fullname:{
+//         userfullname:{
+//             firstname:"Ritu",
+//             lastname:"Raj"
+//         }
+//     }
+// }
+// console.log(regularuser.fullname.userfullname.firstname);
+// const obj1={
+// 1:"a",2:"b"
+// }
+// const obj2={
+// 3:"c",4:"d"
+// }
+// const obj4={obj1,obj2}
+// console.log(obj4)
+// const obj3={...obj1,...obj2}
+// console.log(obj3)
+// const obj5=Object.assign({},obj1,obj2)
+// console.log(obj5)
+console.log(tinderUser.length);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
