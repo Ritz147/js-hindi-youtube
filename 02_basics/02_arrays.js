a666ad26b4b2593652c95aed2c1f59373609944b
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-07 13:55:01 +0000

    array2 commit

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,21 @@
+// const friends=["vani","rishabh","harshit","shreya"]
+// const acq=["manya","mehek","ojasvi"]
+// // friends.push(acq)
+// // console.log(friends)
+// // const arr=friends.concat(acq);
+// // console.log(friends)
+// // console.log(arr)
+// const all=[...friends,...acq,"madhav"]
+// // console.log(all)
+// const arr=[1,2,3,[4,5,6],7,[6,7[4,5]]]
+
+// const arr2=arr.flat(Infinity);
+// console.log(arr2)
+// console.log(Array.isArray("Hitesh"))
+// console.log([..."Ritu"])
+// console.log(Array.from("Ritu"))
+// console.log(Array.from({name:"Ritu"}))//we have to specify whether we want arrays of keys or array of values
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3))
