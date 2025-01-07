449cee191fc5ea28947a870bf16754256343795c
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-07 16:56:46 +0000

    function commit

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,9 @@
+function saymyname(){
+    console.log("Hello World")
+}
+//saymyname()
+//saymyname is just the reference of the function
+function addTwoNumbers(number1,number2){
+    console.log(number1+number2);
+}
+addTwoNumbers(3,null)
