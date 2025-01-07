4f4abe7e4c6abe1a03389c53e6366ba16dda59dd
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-07 07:06:51 +0000

    conversion operator

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,6 +8,27 @@ console.log(valueInNumber);*/
 /*let isLoggedIn="";
 let booleanisLoggedIn=Boolean(isLoggedIn)
 console.log(booleanisLoggedIn)*/
-let someNumber=33;
-let StringNumber=String(someNumber);
-console.log(typeof StringNumber) 
+//let someNumber=33;
+//let StringNumber=String(someNumber);
+//console.log(typeof StringNumber) 
+/*let value =3;
+let negValue=-value;
+console.log(negValue);
+let str1="hello"
+let str2=" hitesh"
+let str=str1+str2;
+console.log(str)*/
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+"2"+2)
+// console.log(1+2+"2")
+//console.log(+false)
+// console.log(+"")
+// let num1,num2,num3
+// num1=num2=num3=2+2
+// console.log(num1)
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
