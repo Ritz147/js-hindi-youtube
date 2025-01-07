80ec7536d413219375a1445d0d72a79e083b5049
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-07 09:25:01 +0000

    string commit

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,26 @@
+const name="Ritu"
+const repocount=50
+//console.log(name+repocount+" Value");
+//string interpolation
+//console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repocount}`);
+const gameName=new String("Ritu-hc")
+// console.log(name[0]);
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(4))
+// console.log(gameName.indexOf('t'));
+// const newString=gameName.substring(-7,4);
+// console.log(newString)
+// const anotherString=gameName.slice(-7,5);
+// console.log(anotherString)
+// const newStringOne="    Ritu    ";
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+// const url="ritz.24.11.2002@gmail.com"
+// console.log(url.replace('.','_'));//replaces the first occurrence
+// console.log(url.includes('ritz'));
+const str='ritz.24.11.2002'
+const words=str.split('.',3);
+console.log(words[0]);
+console.log(words);
