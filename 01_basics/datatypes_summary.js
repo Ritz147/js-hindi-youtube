a708eab1a00e2939e1383df254e1fbb4a4e0a550
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-07 07:43:32 +0000

    datatype_summary commit

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,18 @@
+//  const id=Symbol('123')
+//  const anotherId=Symbol('123')
+//  console.log(id==anotherId)
+//  console.log(id===anotherId)
+//  const bigNumber=3455678890123n
+//  console.log(typeof bigNumber)
+//Arrays,Objects,Functions
+const heroes=["Ritu",'Harshit','Rishabh'];
+let myObj={
+    name:'hitesh',
+    age:22
+}
+const myfunc=function(){
+ console.log("Hello World")
+}
+console.log(typeof heroes)
+console.log(typeof myObj)
+console.log(typeof myfunc)
