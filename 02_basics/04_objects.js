e8ce7d342594744fd8c1773b900a9708943b2a24
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-07 16:44:58 +0000

    object commit

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,8 +27,16 @@ tinderUser.isLoggedIn=false
 // console.log(obj3)
 // const obj5=Object.assign({},obj1,obj2)
 // console.log(obj5)
-console.log(tinderUser.length);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
-console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+// console.log(tinderUser.length);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+const course={
+    coursename:'js in hindi',
+    price:"999",
+    courseInstructor:"Ritu"
+}
+//course.courseInstructor
+const {courseInstructor:instructor}=course
+console.log(instructor)
