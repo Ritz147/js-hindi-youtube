16f3de733c2cb77ad4c14ccaf02f408d7ebfc372
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-07 07:19:58 +0000

    comparison commit

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log("2">1);
+// console.log("02">1);
+// console.log(null>0);//converts null to zero
+// console.log(null==0);
+// console.log(null>=0);//converts null to zero
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+//=== strict check
+console.log("2"==2);
+console.log("2"===2);
