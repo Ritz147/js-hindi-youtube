48699eebd86d5705baf8d5c54ac22c65539356f3
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-07 13:25:22 +0000

    array commit

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,37 @@
+//array
+ const myArr=[0,1,2,3,4,5,true,"hitesh"]
+ //console.log(myArr)
+// const myArr2=new Array(1,2,3,4);
+// console.log(myArr2.length)
+//Array methods
+
+// myArr.push(6)
+// console.log(myArr)
+// myArr.pop()//removes the last element 
+// console.log(myArr)
+// myArr.unshift(0)//inserts array at the start
+// //bekaar hai cause saare elements shift krne padte hain by 1 hence cpu pe load 
+// console.log(myArr)
+// myArr.shift()
+// console.log(myArr)
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))
+// const newArr=myArr.join('');
+// console.log(newArr)
+// console.log(typeof newArr)
+
+//slice,splice
+// console.log(myArr);
+// const myn1=myArr.slice(1,3);
+// console.log("A",myArr);
+// console.log(myn1)
+// const myn2=myArr.splice(1,3);
+// console.log("B",myArr);
+// console.log(myn2);
+const arr=[1,2,3,4]
+// const myarr=arr.splice(1,0,5,6);
+// console.log(arr)
+// console.log(myarr)
+const myarr2=arr.splice(1,1,3)
+console.log(myarr2)
+console.log(arr)
