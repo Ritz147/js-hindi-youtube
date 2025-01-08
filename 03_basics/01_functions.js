5fe864bbe5bf37386434179ecdcda35441e933a3
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-08 07:18:01 +0000

    function commit

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -3,7 +3,25 @@ function saymyname(){
 }
 //saymyname()
 //saymyname is just the reference of the function
+// function addTwoNumbers(number1,number2){
+//     console.log(number1+number2);
+// }
+// const result=addTwoNumbers(3,5)
+// console.log(result)
 function addTwoNumbers(number1,number2){
-    console.log(number1+number2);
+    // console.log("Ritu")
+    // let result=number1+number2;
+    // return result;
+    return number1+number2;
 }
-addTwoNumbers(3,null)
+// const sum=addTwoNumbers(3,5)
+// //console.log(sum)
+// function loginUserMessage(username="sam"){
+//    console.log(`${username} just logged in`);
+// }
+// //console.log(loginUserMessage("Ritu"))
+// loginUserMessage();
+function calculateCartPrice(...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,400,500))
