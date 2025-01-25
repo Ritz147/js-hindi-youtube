8d6747fe3da3de17f2993f0ed855b84df4031fad
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-25 11:10:24 +0000

    get set commit

---
+++ 10_oops/properties_set_get.js
@@ -0,0 +1,7 @@
+function User(email,password){
+    this._email=email;
+    this._password=password;
+    Object.defineProperty(this,'email',{get:function(){return this._email.toUpperCase()},set:function(value){this._email=value}})
+}
+const chai=new User("Ritu@gmail.com","ritu");
+console.log(chai.email)
